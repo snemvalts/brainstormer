2d0022f713b6af6f974a5af7b0878d2d832730e4
Sander Nemvalts <snemvalts@gmail.com>
2014-01-20 16:54:29 +0200

    Further tweaks to the layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+var app = {};
+$(document).ready(function(){
+	app.startButton = $("#startButton")
+	$("#startButton").click(function(){
+		//set visibility:hidden to every element inside jumbotron
+		$(".jumbotron").find("*").each(function(){
+			$(this).toggle();
+		})
+	})
+})
