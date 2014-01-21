1c94cf4a7c2fa8518fd28567852f4a29259eac26
Sander Nemvalts <snemvalts@gmail.com>
2014-01-21 06:51:04 +0200

    Fixed the timer and the displaying of it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,17 +5,20 @@ $(document).ready(function(){
 	app.secondCounter = 0;
 	app.minuteCounter = 0;
 	app.init = function(){
+		app.timer.toggle();
 		window.setInterval(function(){
-		if(app.secondCounter % 59 === 0 && app.secondCounter !== 0){
-			app.minuteCounter++;
-			app.secondCounter = 0;
-		}
-		else{
-			app.secondCounter++;
-		}
-		app.timer.html(app.minuteCounter+":"+app.secondCounter);
-		console.log(app.minuteCounter+":"+app.secondCounter)
+			if(app.secondCounter === 59){
+				//set to -1, because nevertheless it will increment it(we want 01:00)
+				app.secondCounter = -1;
+				app.minuteCounter++
+			}
+		app.secondCounter++
+		console.log(app.prefix0(app.minuteCounter)+":"+app.prefix0(app.secondCounter))
+		app.timer.text(app.prefix0(app.minuteCounter)+":"+app.prefix0(app.secondCounter))
 		},1000)
+	}
+	app.prefix0 = function(number){
+		return number < 10? "0"+number: number;
 	}
 	$("#startButton").click(function(){
 		//set visibility:hidden to every element inside jumbotron
