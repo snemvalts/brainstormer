73bd641abc21e820d0e6f868786c21ac98b96fac
Sander Nemvalts <snemvalts@gmail.com>
2014-01-21 18:57:26 +0200

    Set up an input box and baseline for the app itself

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,15 +10,22 @@ $(document).ready(function(){
 			if(app.secondCounter === 59){
 				//set to -1, because nevertheless it will increment it(we want 01:00)
 				app.secondCounter = -1;
-				app.minuteCounter++
+				app.minuteCounter++;
 			}
 		app.secondCounter++
 		console.log(app.prefix0(app.minuteCounter)+":"+app.prefix0(app.secondCounter))
 		app.timer.text(app.prefix0(app.minuteCounter)+":"+app.prefix0(app.secondCounter))
-		},1000)
+		},1000);
+
 	}
 	app.prefix0 = function(number){
 		return number < 10? "0"+number: number;
+	}
+	app.inputManager = function(keypress){
+		console.log(keypress.which)
+		if(keypress.which == 13){
+			console.log("enter has been pressed")
+		}
 	}
 	$("#startButton").click(function(){
 		//set visibility:hidden to every element inside jumbotron
@@ -26,5 +33,8 @@ $(document).ready(function(){
 			$(this).toggle();
 		})
 		app.init();
-	})
+	});
+	$("#ideaInput").keypress(function(keypress){
+		app.inputManager(keypress)
+	});
 })
